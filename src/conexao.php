34331eb132dc21f30bdd50cf0efcdc9e833a281d
NelsonSilva <nelsonolech@gmail.com>
2023-04-22 15:11:08 -0300

    teste

---
+++ src/conexao.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "jobstage";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Conexão falhou: " . mysqli_connect_error());
+}
---
+++ src/inserir-dados.php
@@ -0,0 +1,61 @@
+<?php
+// Verifica se os dados foram submetidos
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Conecta-se ao banco de dados
+    include("conexao.php");
+
+    // Recupera os dados do formulário
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+
+
+
+
+
+
+    // query que faz o join de todas as tabelas do banco
+    $sql_join = "SELECT * from usuario as U
+    inner join usuario_formacao as UF
+    on U.ID_USUARIO = UF.ID_USUARIO
+    INNER JOIN FORMACAO AS F
+    ON F.ID_FORMACAO = UF.ID_FORMACAO
+    INNER JOIN USUARIO_EXPERIENCIA AS UE
+    ON UE.ID_USUARIO = U.ID_USUARIO
+    INNER JOIN EXPERIENCIA AS E
+    ON E.ID_EXPERIENCIA = UE.ID_EXPERIENCIA
+    INNER JOIN USUARIO_ESCOLARIDADE AS UES
+    ON UES.ID_USUARIO = U.ID_USUARIO
+    INNER JOIN ESCOLARIDADE AS ES
+    ON ES.ID_ESCOLARIDADE = UES.ID_ESCOLARIDADE
+    INNER JOIN USUARIO_CURSO_EXTRA AS UCX
+    ON UCX.ID_USUARIO = U.ID_USUARIO
+    INNER JOIN CURSO_EXTRA AS CX
+    ON CX.ID_CURSO = UCX.ID_CURSO_EXTRA";
+
+
+    // fazer insert inserindo as chaves estrangeiras de acordo com o id do usuario 
+
+    // Prepara a consulta SQL
+    $sql = "INSERT INTO usuarios (nome, email) VALUES ('$nome', '$email')";
+
+
+
+
+
+
+
+
+
+
+
+    // Executa a consulta SQL
+    if (mysqli_query($conn, $sql)) {
+        echo "Dados inseridos com sucesso!";
+    } else {
+        echo "Erro ao inserir dados: " . mysqli_error($conn);
+    }
+
+    // Fecha a conexão com o banco de dados
+    mysqli_close($conn);
+}
+?>
