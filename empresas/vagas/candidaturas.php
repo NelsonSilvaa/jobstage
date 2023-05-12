00c5a6ac2a33c59836ac977f62d61366f701ceea
NelsonSilva <nelsonolech@gmail.com>
2023-05-12 13:02:48 -0300

    criando função para ver candidaturas V1.0

--- empresas/vagas/candidaturas.php
+++ empresas/vagas/candidaturas.php
@@ -4,7 +4,7 @@ session_start();
     if(empty($_SESSION)){
         header("Location: ../index.html");
     }
-    include("../src/configs/conexao.php");
+    include("../../src/configs/conexao.php");
 
     $empresa_id = $_SESSION['ID_EMPRESA'];
 
@@ -18,10 +18,10 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Candidaturas</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/index.css">
-    <link rel="stylesheet" href="../css/layout.css">
-    <link rel="stylesheet" href="../css/sweetalert2.css">
-    <link rel="stylesheet" href="../css/validacoes.css">
+    <link rel="stylesheet" href="../../css/index.css">
+    <link rel="stylesheet" href="../../css/layout.css">
+    <link rel="stylesheet" href="../../css/sweetalert2.css">
+    <link rel="stylesheet" href="../../css/validacoes.css">
     <meta http-equiv="Cache-Control" content="no-cache" />
 </head>
 <body>
@@ -46,23 +46,95 @@ session_start();
       </ul>
         </ul>
     </div>
+</div>
+<div class="container container-vagas">
 
-    <div class="container-dados">
-        <h1>SEJA BEM VINDO A JOBSTAGE!!!</h1>
-        <p>complete seu perfil na aba de dados para poder visualizar novas vagas de estágio!</p>
-    </div>
-    
-    
+
+    <?php
+
+    $resultado = mysqli_query($conn, "SELECT V.NOME AS VNOME, U.NOME AS UNOME FROM empresa AS E
+    INNER JOIN vagas AS V ON E.ID_EMPRESA = V.ID_EMPRESA
+    INNER JOIN usuario_vagas UV ON UV.ID_VAGA = V.ID_VAGA
+    INNER JOIN usuario as U ON UV.ID_USUARIO = U.ID_USUARIO
+    WHERE E.ID_EMPRESA = $empresa_id");
+
+    $vagas_unicas = array();
+
+    // Itere sobre o resultado da consulta SQL
+    while ($row = mysqli_fetch_array($resultado)) {
+    // Crie uma chave única para cada vaga com base no nome da vaga
+    $chave = $row['VNOME'];
+
+    // Adicione a vaga ao array de vagas únicas se ela ainda não estiver presente
+    if (!array_key_exists($chave, $vagas_unicas)) {
+        $vagas_unicas[$chave] = array(
+            'nome_vaga' => $row['VNOME'],
+            'nomes_usuarios' => array()
+        );
+    }
+
+    // Adicione o nome do usuário ao array de nomes de usuário para esta vaga
+    $vagas_unicas[$chave]['nomes_usuarios'][] = $row['UNOME'];
+    }
+
+    // Itere sobre o array de vagas únicas para exibir os resultados
+    $idModal = 1;
+    foreach ($vagas_unicas as $vaga) {
+        print '<div class="" data-id= data-row-id=>
+
+                    <div class="card">
+                        <div class="card-titulo">
+                            <h2>'.$vaga['nome_vaga'].'</2>
+                        </div>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalCandidatos'.$idModal.'">
+                            Ver candidaturas
+                        </button> 
+                    </div>
+                </div>';
+
+    // Exiba os nomes dos usuários que se candidataram a esta vaga
+
+        print ' <div class="modal fade" id="modalCandidatos'.$idModal.'" tabindex="-1" role="dialog" aria-labelledby="modalCandidatos'.$idModal.'" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5>CANDIDATURAS</h5>
+                        </div>
+                        <div class="modal-body">
+                        <table>';
+                            foreach ($vaga['nomes_usuarios'] as $nome_usuario) {
+                                print'  
+                                            <tr>
+                                                <td> '. $nome_usuario .'</td>
+                                            </tr>
+                                        ';
+                            
+                            }
+                print  '</table>
+                </div>
+                    </div>
+                    </div>
+                </div>';
+
+        $idModal += 1;
+    }
+
+
+    ?>
 </div>
 
 
-    <script src="../src/JS/jquery-3.6.4.js"></script>
-    <script src="../src/JS/swetalert2.js"></script>
-    <script 
-        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
-        integrity="sha384-kSpN/7CfdBjN9+RY5DhN5Hz5zr+ZnysK8W1ufX0ZN0SPR20BpZiDgmWwfdKvSGtl"
-        crossorigin="anonymous">
-    </script>
+
+
+
+
+
+
+
+    <script src="../../src/JS/jquery-3.6.4.js"></script>
+    <script src="../../src/JS/processos.js"></script>
+    <script src="../../src/JS/swetalert2.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-kSpN/7CfdBjN9+RY5DhN5Hz5zr+ZnysK8W1ufX0ZN0SPR20BpZiDgmWwfdKvSGtl" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
--- usuario/candidaturas.php
+++ usuario/candidaturas.php
@@ -8,7 +8,24 @@ session_start();
 
     $user_id = $_SESSION['ID_USUARIO'];
 
+    $sql= "SELECT E.NOME AS ENOME, V.ID_VAGA AS VVAGA, V.TURNO AS VTURNO, V.TURNO_DAS AS VTURNO_DAS, V.TURNO_ATE AS VTURNO_ATE, 
+                  V.SALARIO AS VSALARIO, V.CIDADE AS VCIDADE, V.ESTADO AS VESTADO, V.TIPO_CONTRATO AS VTIPO_CONTRATO, 
+                  V.REQUISITOS AS VREQUISITOS, V.DESCRICAO AS VDESCRICAO, V.NOME AS VNOME
+            FROM empresa AS E
+            INNER JOIN vagas AS V
+            ON V.ID_EMPRESA = E.ID_EMPRESA
+            WHERE V.ID_VAGA IN (SELECT ID_VAGA FROM usuario_vagas WHERE ID_USUARIO = $user_id)";
+
+    $result = $conn->query($sql);
+
+    // Verifica se houve erro na consulta
+    if (!$result) {
+        die("Erro na consulta: " . $conn->error);
+    }
+
 ?>
+
+
 <!DOCTYPE html>
 <html lang="pt_BR">
 <head>
@@ -19,8 +36,10 @@ session_start();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/index.css">
     <link rel="stylesheet" href="../css/layout.css">
+    <link rel="stylesheet" href="../css/sweetalert2.css">
+    <link rel="stylesheet" href="../css/validacoes.css">
     <meta http-equiv="Cache-Control" content="no-cache" />
-    <script src="../../src/JS/jquery-3.6.4.js"></script>
+    <script src="../src/JS/jquery-3.6.4.js"></script>
 </head>
 <body>
 <header>
@@ -40,41 +59,98 @@ session_start();
             <li style="background-color: red;"> <a href="../src/configs/logout.php">Sair</a></li>
         </ul>
     </div>
-
-    <div class="container-dados">
-        <h1>Candidaturas</h1>
-    </div>
-    
-    
+    <div class="container container-vagas">
+    <?php
+        // Percorre os resultados da consulta
+        $id_table = 1;
+        while ($row = $result->fetch_assoc()) {
+     print ' <div class="" data-id='.$id_table.' data-row-id='.$row['VVAGA'].'>
+                <div class="card">
+                    <div class="card-titulo">
+                        <h2>'.$row['VNOME'].'</2>
+                    </div>
+                    
+                    <div class="titulo-conteudo-prev">
+                        <h3>'.$row['ENOME'].'</h3>
+                    </div>
+                    <div class="card-conteudo-prev">
+                        <div class="cidade">
+                            '.$row['VCIDADE'].' -
+                        </div>
+                        <div class="turno">
+                            <div class="turnoPeriodo">
+                                '.$row['VTURNO'].' 
+                            </div>
+                            <div class="tDas">
+                                :'.$row['VTURNO_DAS'].'
+                            </div>
+                            <div class="tAte">
+                            -'.$row['VTURNO_ATE'].'
+                            </div>
+                        </div>
+                        <div class="tipo-Contrato">
+                            - '.$row['VTIPO_CONTRATO'].'
+                        </div>
+                    </div>
+                    <div class="card-conteudo-prev_down">
+                        <div class="salario">
+                            R$ '.$row['VSALARIO'].'
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#detalheVagas'.$row['VVAGA'].'" aria-expanded="false" aria-controls="detalheVagas">
+                                Detalhes
+                            </button>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="collapse" id="detalheVagas'.$row['VVAGA'].'">
+                                <div class="content-vagas">
+                                    <div class="descricao">
+                                        <div class="card-titulo titulo-descricao">
+                                            <h4>DESCRIÇÃO</h4>
+                                        </div>
+                                        <div class="descricao-conteudo">
+                                        '.$row['VDESCRICAO'].'
+                                        </div>
+                                    </div>
+                                    <div class="requisitos">
+                                        <div class="card-titulo titulo-requisitos">
+                                            <h4>REQUISITOS</h4>
+                                        </div>
+                                        <div class="requisitos-conteudo">
+                                        '.$row['VREQUISITOS'].'
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+            </div>';
+        }
+    $conn->close();
+?>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-<div class="container">
+<!-- exemplo de info -->
+    <!-- <div class="container-dados">
+        <div class="container">
             <div class="row">
                 <div class="col-sm-12">
                     <div class="card">
                         <div class="card-header">
-                        <h5 class="card-title">NOME DA EMPRESA</h5>
+                        <h5 class="card-title">NOME DA VAGA</h5>
                         </div>
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-sm-12">
                                 <ul>
-                                    <li>Setor:{informação do banco}</li>
+                                    <li>Nome Empresa:{informação do banco}</li>
                                     <li>Turno:{informação do banco}</li>
                                     <li>Bairro:{informação do banco}</li>
-                                    <li>Bolsa:{informação do banco}</li>
+                                    <li>Salario:{informação do banco}</li>
                                     <li>beneficio:VT/VR/VA/Home-Office/Hibrido/Day Off</li>
                                 </ul>
                                 </div>
@@ -82,6 +158,7 @@ session_start();
                             <div class="row">
                                 <div class="col-sm-12">
                                 <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#aaa" aria-expanded="false" aria-controls="aaa">
+                                    Detalhes
                                 </button>
                                 </div>
                             </div>
@@ -91,15 +168,18 @@ session_start();
                                         <div class="content-vagas">
                                             <div class="descricao">
                                                 <div class="titulo-descricao">
-                                                    
+                                                    <h4>DESCRIÇÃO</h4>
                                                 </div>
                                                 <div class="descricao-conteudo">
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut non repudiandae numquam magni minus, alias maxime quidem incidunt corrupti suscipit asperiores neque nemo officiis laborum! Fugiat recusandae minus inventore quam.
                                                 </div>
                                             </div>
                                             <div class="requisitos">
                                                 <div class="titulo-requisitos">
+                                                    <h4>REQUISITOS</h4>
                                                 </div>
                                                 <div class="requisitos-conteudo">
+                                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut non repudiandae numquam magni minus, alias maxime quidem incidunt corrupti suscipit asperiores neque nemo officiis laborum! Fugiat recusandae minus inventore quam.
                                                 </div>
                                             </div>
                                         </div>
@@ -111,39 +191,18 @@ session_start();
                 </div>
             </div>
         </div>
+    </div> -->
+</div>
 
+         
 
+       
+    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <script 
-        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
-        integrity="sha384-kSpN/7CfdBjN9+RY5DhN5Hz5zr+ZnysK8W1ufX0ZN0SPR20BpZiDgmWwfdKvSGtl"
-        crossorigin="anonymous">
-    </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="../src/JS/swetalert2.js"></script>
+<script src="../src/JS/processos.js"></script>
+<script src="../src/JS/jquery-3.6.4.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-kSpN/7CfdBjN9+RY5DhN5Hz5zr+ZnysK8W1ufX0ZN0SPR20BpZiDgmWwfdKvSGtl" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
