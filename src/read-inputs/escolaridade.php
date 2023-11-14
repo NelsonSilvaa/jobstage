f347c3755a1522c1effb2db2fc74880b6ab19f63
NelsonSilvaa <nelsonolech@gmail.com>
2023-11-14 20:54:03 -0300

    removendo parte do codigo que le arquivo

--- src/read-inputs/escolaridade.php
+++ src/read-inputs/escolaridade.php
@@ -16,21 +16,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $prev_formatura = $_POST['prev-formatura'];
     $periodo = $_POST['periodo'];
     $duracao = $_POST['duracao'];
-    $declaracao = $_FILES['declaracao'];
-
-    if($declaracao != null){
-        preg_match("/\.(pdf){1}$/i", $declaracao['name'], $ext);
-
-        if($ext == true){
-            $nomeArquivo = md5(uniqid(time())) . "." . $ext[1];
-
-
-            $caminhoArquivo = "../../uploads/" . $nomeArquivo; 
-
-            move_uploaded_file($declaracao['tmp_name'], $caminhoArquivo);
-
-        }
-    }
 
     $sqlEscolaridade = "INSERT INTO escolaridade (CURSO, INSTITUICAO, CONTATO, TURNO, PREVISAO_FORMATURA, PERIODO, DURACAO, DECLARACAO_MATRICULA, ID_USUARIO)
                         VALUES ('$curso_escolaridade', '$instituicao_escolaridade', '$contato', '$turno', '$prev_formatura', '$periodo', '$duracao', '$nomeArquivo', '$user_id')";
