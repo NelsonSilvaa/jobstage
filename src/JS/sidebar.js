2ff2f8d29b418db9a4dc732752492067de4882d9
NelsonSilvaa <nelsonolech@gmail.com>
2023-12-04 01:46:25 -0300

    corrigindo novos links do bootstraap

--- src/JS/sidebar.js
+++ src/JS/sidebar.js
@@ -1,7 +1,21 @@
-$(".sidebar ul li").on('click', function () {
+var url = window.location.href;
+
+if (url.includes("index.php")) {
     $(".sidebar ul li.active").removeClass('active');
-    $(this).addClass('active');
-});
+    $(".inicio").addClass('active');
+}else if (url.includes("dadosUser.php") || url.includes("formacaoUser.php")  || url.includes("cursoUser.php") || url.includes("expUser.php")) {
+    $(".sidebar ul li.active").removeClass('active');
+    $(".dados").addClass('active');
+}else if (url.includes("vagas.php")) {
+    $(".sidebar ul li.active").removeClass('active');
+    $(".vagas").addClass('active');
+}else if (url.includes("candidaturas.php")) {
+    $(".sidebar ul li.active").removeClass('active');
+    $(".candidaturas").addClass('active');
+}else if (url.includes("curriculo.php")) {
+    $(".sidebar ul li.active").removeClass('active');
+    $(".curriculo").addClass('active');
+}
 
 $('.open-btn').on('click', function () {
     $('.sidebar').addClass('active');
